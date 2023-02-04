c4cd6e1f5a16f7df90d57aa8aed6b32860175c9e
khkh0109 <77181642+khkh0109@users.noreply.github.com>
2023-02-04 23:51:50 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,14 @@
+function solution(price) {
+    let result = 0;
+    if(price >= 500000) {
+        result = price * 0.8;
+    } else if(price >= 300000) {
+        result = price * 0.9;
+    } else if(price >= 100000) {
+        result = price * 0.95;
+    } else {
+        return price;
+    }
+    
+    return Math.trunc(result);
+}
