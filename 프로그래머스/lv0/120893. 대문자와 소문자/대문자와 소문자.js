7b145cab55db252f98e9003c10a2d10e6279279f
khkh0109 <77181642+khkh0109@users.noreply.github.com>
2023-02-08 22:18:19 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.27 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,8 +1,7 @@
 function solution(my_string) {
     let result = ''
     for(let i = 0; i < my_string.length; i++) {
-        const lowerCase = /[a-z]/g.test(my_string[i]);
-        if(lowerCase) { 
+        if(my_string[i] === my_string[i].toLowerCase()) { 
             result += my_string[i].toUpperCase();
         } else {
             result += my_string[i].toLowerCase();
