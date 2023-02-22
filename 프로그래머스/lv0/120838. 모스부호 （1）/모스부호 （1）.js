451ec0775394e6bba4a71812a972c69aac3f0db4
khkh0109 <77181642+khkh0109@users.noreply.github.com>
2023-02-22 22:29:07 +0900

    [level 0] Title: 모스부호 (1), Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,21 @@
+function solution(letter) {
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    
+    let result = '';
+    
+    const letterSplit = letter.split(' ');
+    
+    letterSplit.forEach(word => {
+        if(word in morse) {
+            result += morse[word];
+        }
+    });
+    
+    return result;
+}
