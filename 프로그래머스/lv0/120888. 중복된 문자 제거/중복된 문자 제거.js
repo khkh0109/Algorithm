83b93f721c7db20cfdeb223e164279967b187c14
khkh0109 <77181642+khkh0109@users.noreply.github.com>
2023-04-09 13:20:57 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,10 +1,4 @@
 function solution(my_string) {
-    let result = '';
-    
-    for(let i = 0; i < my_string.length; i++) {
-        const isInclude = result.includes(my_string[i]);
-        result += isInclude ? '' : my_string[i];     
-    }
-    
-    return result;
+    const result = new Set(my_string);
+    return [...result].join('');
 }
